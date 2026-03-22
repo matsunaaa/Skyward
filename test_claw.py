ce69dd06c4324fba24603d15979f81c895b78900
Emily Wu <163802613+matsunaaa@users.noreply.github.com>
2026-03-22 09:58:21 -0700

    change servo angle

--- test_claw.py
+++ test_claw.py
@@ -8,9 +8,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-# Initialize both servos as 270-degree
+# Initialize servo as 270-degree
 claw_servo = servo.Servo(pca.channels[0], actuation_range=270, min_pulse=500, max_pulse=2500)
-winch_servo = servo.Servo(pca.channels[1], actuation_range=270, min_pulse=500, max_pulse=2500)
 
 # Center both servos to start
 claw_servo.angle = 135
@@ -19,25 +18,26 @@ time.sleep(1)
 
 try:
     while True:
-        # Close the claw
-        print("Claw: CLOSING (Moving to 160 deg)")
+        #wide open
+        print("Claw:110 deg")
+        claw_servo.angle = 110
+        time.sleep(2)
+        
+        print("Claw:160 deg")
         claw_servo.angle = 160 
         time.sleep(2)
 
-        # Lower the winch
-        print("Winch: LOWERING (Moving to 0 deg)")
-        winch_servo.angle = 0
-        time.sleep(3) 
-
-        # Open the claw
-        print("Claw: OPENING (Moving to 110 deg)")
-        claw_servo.angle = 110
+        print("Claw:200 deg")
+        claw_servo.angle = 200 
         time.sleep(2)
 
-        # Raise the winch
-        print("Winch: RAISING (Moving to 270 deg)")
-        winch_servo.angle = 270
-        time.sleep(3)
+        print("Claw:220 deg")
+        claw_servo.angle = 220 
+        time.sleep(2)
+
+
+
+        
 
 except KeyboardInterrupt:
     print("\nTest stopped by user.")
